1f4ff68a9275eb295dbbe9915327863235713ed6
lesson6 <puzzleuri@mail.ru>
2020-09-16 18:35:00 +0300

    ok

---
+++ chat_fx_03092020/src/server/Server.java
@@ -0,0 +1,81 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    private int PORT = 8189;
+    ServerSocket server = null;
+    Socket socket = null;
+
+    public Server(){
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg){
+        String message = String.format("%s : %s", sender.getNickname(), msg);
+        String[] m = msg.split("\\s", 3);
+        for (ClientHandler c : clients) {
+            if (m[0].equals("/w")){
+                if (m.length < 3){
+                    sender.sendMsg("Сообщение введено некорректно");
+                    break;
+                }
+            if (m[1].equals(c.getNickname())){
+                c.sendMsg("приватно от " + sender.getNickname()  + ": "+ m[2]);
+                if(!c.equals(sender)){
+                    sender.sendMsg(sender.getNickname() + ": " + m[2]);
+                }
+                return;
+            } else
+                sender.sendMsg("Такой Nickname не найден");
+            } else
+            c.sendMsg(message);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
+}
